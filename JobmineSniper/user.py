611299492266e812c0962d907f186c3011a92444
Hanson Wang <hanson.wng@gmail.com>
2011-01-21 22:21:49 -0500

    proper serialization

--- JobmineSniper/user.py
+++ JobmineSniper/user.py
@@ -1,19 +1,20 @@
 from encrypt import *
+import cPickle as pickle
 
 class User:
-  def __init__(self, passkey=''):
-    self.passkey = passkey
+  def __init__(self):
     self.username = ''
     self.password = ''
+    self.email = ''
 
-  def load(self, fileName):
-    o = open(fileName, 'r')
-    self.username = decrypt(o.readline()[:-1], self.passkey)
-    self.password = decrypt(o.readline()[:-1], self.passkey)
-  
-  def save(self, fileName):
+  def save(self, fileName, passkey):
+    data = pickle.dumps(self)
+    data = encrypt(data, passkey)
     o = open(fileName, 'w')
-    o.write(
-        encrypt(self.username, self.passkey) + '\n' +
-        encrypt(self.password, self.passkey) + '\n'
-    )
+    o.write(data)
+
+def loadUser(fileName, passkey):
+  o = open(fileName, 'r')
+  data = o.read()
+  data = decrypt(data, passkey)
+  return pickle.loads(data)
