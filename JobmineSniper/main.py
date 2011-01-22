ce7bded947414ade57e8bf0e3622477e04e3e347
Hanson Wang <hanson.wng@gmail.com>
2011-01-21 23:17:54 -0500

    basic thread

--- JobmineSniper/main.py
+++ JobmineSniper/main.py
@@ -1,30 +1,71 @@
 from Tkinter import *
+from user import *
+from scraper import *
+from threading import Thread
 
 class JobmineSniper(Frame):
-    def say_hi(self):
-        print "hi there, everyone!"
+    def check(self):
+      i = 0
+      while self.go:
+        self.label.config(text = str(i))
+        i += 1
+        page = self.scraper.getApplicationPage()
+        #newStatus = self.parser.getAppStatus(page)
+        #if user.appStatus != newStatus:
+        #  pass
+    def startLoop(self):
+      self.scraper = Scraper(self.user)
+      self.thread = Thread(target=self.check)
+      self.go = True
+      self.thread.start()
+    def stopLoop(self):
+      self.go = False
+      if self.thread:
+        self.thread.join()
+      self.label.config(text = 'stopped')
+    def loginInfo(self):
+      pass
+    def notifyInfo(self):
+      pass
 
     def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
+        self.login = Button(self)
+        self.login["text"] = "Credentials..."
+        self.login["command"] = self.loginInfo
+        self.login.pack()
 
-        self.QUIT.pack({"side": "left"})
+        self.notify = Button(self)
+        self.notify["text"] = "Notify me..."
+        self.notify["command"] = self.notifyInfo
+        self.notify.pack()
 
-        self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
+        self.label = Label(self)
+        self.label["text"] = "Nothing's happened"
+        self.label.pack()
 
-        self.hi_there.pack({"side": "left"})
+        self.start = Button(self)
+        self.start["text"] = "Start"
+        self.start["fg"] = "green"
+        self.start["command"] = self.startLoop
+
+        self.start.pack(side=LEFT, fill=X)
+
+        self.stop = Button(self)
+        self.stop["text"] = "Stop",
+        self.stop["fg"] = "red"
+        self.stop["command"] = self.stopLoop
+
+        self.stop.pack({"side": "left"})
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        Frame.__init__(self, master, width=200, height=200)
+        self.pack_propagate(0)
         self.pack()
         self.createWidgets()
+        self.thread = False
+        self.user = loadUser('user.txt', '1234')
 
 root = Tk()
+root.resizable(0, 0)
 app = JobmineSniper(master=root)
 app.mainloop()
-root.destroy()
-
