d047d8f7947676eafe35ebf4178399d9ba14a560
Hanson Wang <hanson.wng@gmail.com>
2011-01-21 19:27:12 -0500

    update

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import urllib, urllib2, cookielib
 
-username = raw_input('Username:')
-password = raw_input('Password:')
+username = raw_input()
+password = raw_input()
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 login_data = urllib.urlencode({'userid' : username, 'pwd' : password})
 opener.open('https://quest.pecs.uwaterloo.ca/psp/SS/?cmd=login', login_data)
-select_data = urllib.urlencode({'SSR_DUMMY_RECV1$sels$0' : 1})
+
+print 'Logged in'
+
+resp = opener.open('https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL')
+
+select_data = urllib.urlencode({
+  'ICAction': 'DERIVED_SSS_SCT_SSR_PB_GO',
+  'DERIVED_SSTSNAV_SSTS_MAIN_GOTO$24$': 9999,
+  'SSR_DUMMY_RECV1$sels$0' : 0,
+  'DERIVED_SSTSNAV_SSTS_MAIN_GOTO$68$': 9999,
+})
+
 resp = opener.open('https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL', select_data)
 
 o = file('x.html', 'w')
